ed83a5dde56e945189177bbcbf91d212ef6a30f9
paullaster <paullasterokoth98@gmail.com>
2022-10-14 12:31:30 +0300

    styling the star

--- index.js
+++ index.js
@@ -9,18 +9,20 @@ function ratingStarClickHandler (index, event) {
     if (event){
         let star = event.target.parentElement.children;
          for (let j = 0; j <star.length; j++) {
-             star[j].style.backgroundColor = 'transparent';
+             star[j].classList.remove ( 'orange' );
          }
     }
     if (event.target.previousElementSibling !== null) {
         for (let j = 0; j <= index; j++) {
-            ratingStarElement ( 'star')[j].style.backgroundColor = '#B58B00';
+            ratingStarElement ( 'star')[j].classList.add ( 'orange');
         }
     }else {
-        event.target.style.backgroundColor = '#B58B00'
+        event.target.classList.add ( 'orange');
     }
     let userRating = index + 1;
-    
+    getELementByClass ( 'output').style.paddingRight = 0.5 + 'em';
+    getELementByClass ( 'output').style.paddingLeft = 0.5 + 'em';
+    getELementByClass ( 'output').style.borderRadius = 0.2 + 'em';
     addTextContent ( 'output', userRating);
 }
 function ratingStarElement (classname) {
