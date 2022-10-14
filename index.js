534e35aaa24cce5f7c132ff58eef24640e4e0096
paullaster <paullasterokoth98@gmail.com>
2022-10-14 07:40:19 +0300

    rating

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
-document.addEventListener ('DOMContentLoaded', ratingStarsClickHandler);
-function ratingStarsClickHandler () {
+document.addEventListener ('DOMContentLoaded', ratingStars);
+function ratingStars () {
     for (let i = 0; i < ratingStarElement ( 'star').length; i++) {
-        console.log (ratingStarElement ( 'star')[i]);
+        ratingStarElement ( 'star')[i].addEventListener ( 'click', ratingStarClickHandler.bind(null, i));
     };
+}
+function ratingStarClickHandler (index, event) {
+    let userRating = index + 1;
+    
+    addTextContent ( 'output', userRating);
 }
 function ratingStarElement (classname) {
     return document.querySelectorAll ( '.' + classname );
 };
+function getELementByClass (classname) {
+    return document.querySelector ( '.' + classname );
+}
+function addTextContent (classname, text)  {
+    return getELementByClass (classname).textContent = `You rated this ${text === 1 ? text + ' star' :  text + ' stars' }`;
+}
