acef9994904a09291b1bafa80e88229dd4cde41b
paullaster <paullasterokoth98@gmail.com>
2022-10-14 08:00:48 +0300

    adding bacnground to the star

--- index.js
+++ index.js
@@ -5,6 +5,14 @@ function ratingStars () {
     };
 }
 function ratingStarClickHandler (index, event) {
+    console.log (event.target.previousElementSibling);
+    if (event.target.previousElementSibling !== null) {
+        for (let j = 0; j <= index; j++) {
+            ratingStarElement ( 'star')[j].style.backgroundColor = '#B58B00';
+        }
+    }else {
+        event.target.style.backgroundColor = '#B58B00'
+    }
     let userRating = index + 1;
     
     addTextContent ( 'output', userRating);
